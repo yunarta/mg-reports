2056c974bf22f708e3ef9cabbb3dae2843c4a582
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2025-03-08 00:39:58 +0800

    Add default Sphinx extensions configuration
    The `extensions` variable was removed as it was set to an empty list, which is unnecessary. This simplifies the configuration and aligns it with Sphinx's default behavior.

--- publication/source/conf.py
+++ publication/source/conf.py
@@ -20,8 +20,6 @@ author = 'Yunarta Kartawahyudi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
 
